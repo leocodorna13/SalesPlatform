b1397457448d0ced8e4e2bf43a9592d962211c04
leocodorna13 <leo.martinsntep@gmail.com>
2025-03-20 01:57:35 -0300

    search products

--- src/middleware.js
+++ src/middleware.js
@@ -1,2 +1,7 @@
 // Este arquivo está sendo removido pois não vamos usar o middleware do Supabase
 // Estamos usando a opção mais simples com persistSession no createClient
+
+export function onRequest(context, next) {
+  // Middleware básico que apenas passa a requisição adiante
+  return next();
+}
